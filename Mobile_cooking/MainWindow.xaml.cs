9b23ea2f96c4362705faa7cde7856cb886e09a26
Sveta1717 <akai.iti@gmail.com>
2023-01-20 21:07:18 +0200

    2

--- Mobile_cooking/MainWindow.xaml.cs
+++ Mobile_cooking/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+using Mobile_cooking.Models;
+using System.Data.Common;
 
 namespace Mobile_cooking
 {
@@ -20,9 +24,118 @@ namespace Mobile_cooking
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection connection;
+        //public ObservableCollection<ListViewItem> listItems { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            String connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PC\source\repos\Mobile_cooking\Mobile_cooking\Database1.mdf;Integrated Security=True";
+            connection = new SqlConnection (connectionString);
+        }
+
+        public void ListReciept(String reciept)
+        {
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                List<Book> list = new List<Book>();
+                //cmd.CommandText = reciept;
+                SqlDataReader res = cmd.ExecuteReader();
+
+                while (res.Read())
+                {
+                    list.Add(new Book()
+                    {
+                        Id = res.GetInt32(0),
+                        Name = res.GetString(1),
+                        Description = res.GetString(2)
+                    });
+                }
+                res.Close();
+                
+            }
+        }
+
+        private void FirstButon_Click(object sender, RoutedEventArgs e)
+        {
+            ListReciept(Name);            
+        }
+
+        private void SecondButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaladButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CakeButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SouseButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DesertButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DrinkButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FoundButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ServiceButon_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Розроблювачі: Соломон Юрій та Сироткіна Світлана");
+        }
+
+        private void QuestionButon_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void MenuButon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButon_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListViewItem item)
+            {                
+                Description.Text = item.Content as string;
+                Ingredients.Text = "1233";
+                Image.FindName("");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
         }
     }
 }
---
+++ Mobile_cooking/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_cooking.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Book(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public Book()
+        {
+        }
+    }
+}
